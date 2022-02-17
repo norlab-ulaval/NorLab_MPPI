1a2e6c83d2b00f2ff18dd123514b2013afe808b2
DomBaril <dominic.baril@usherbrooke.ca>
2022-02-17 14:04:56 -0500

    NLMPPI-25 Implement Model class

--- src/barebones_mpc/sampler/std_dev_sampler.py
+++ src/barebones_mpc/sampler/std_dev_sampler.py
@@ -8,6 +8,9 @@ class StandardDevSampler(AbstractSampler):
         self.sample_length = sample_length
         self.init_state = init_state
         self.std_dev = std_dev
+        self.sample_input = np.empty((self.sample_length, self.number_samples+1, self.input_dimension))
+
+        self.model = model
 
     def sample_inputs(self, nominal_input):
         """ Sample inputs based on the nominal input array
@@ -15,10 +18,10 @@ class StandardDevSampler(AbstractSampler):
         :param nominal_input: the nominal input array
         :return: sample input array
         """
-        sample_noise = np.random.normal(loc=0, scale=self.std_dev, size=(self.input_dimension, self.number_samples))
-        sample_input = np.sum(nominal_input, sample_noise)
-        sample_input = np.concatenate(nominal_input, sample_input, axis=0)
-        return sample_input
+        self.sample_input[:, 0, :] = nominal_input
+        sample_noise = np.random.normal(loc=0, scale=self.std_dev, size=(self.sample_length, self.number_samples, self.input_dimension))
+        self.sample_input[:, 1:, :] = sample_noise + np.tile(nominal_input.reshape(self.sample_length, 1, self.input_dimension), (1, self.number_samples, 1))
+        return self.sample_input
 
     def sample_states(self, sample_input, init_state):
         """ Sample states based on the sample input array through the model
---
+++ tests/test_barebones_mpc/test_barebones_mpc_inv_pend_model.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+
+import pytest
+import numpy as np
+from src.barebones_mpc.model.inv_pendulum_model import InvPendulumModel
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+@pytest.fixture
+def model_init_params():
+    time_step = 1/20  # 20 hz or 0.2 seconds
+    commanded_lon_vel = 1.5  # m/s
+    horizon = 0.75  # s
+    sample_length = int(horizon/time_step)
+    number_samples = 1000
+    input_dimension = 1 # input array dimension
+    state_dimension = 4 # state array dimension
+    init_state = np.zeros(4) # initial state vector
+    cart_mass = 1 # kg
+    pendulum_mass = 1 # kg
+    sample_input = np.random.normal(loc=0, scale=1, size=(sample_length, number_samples, input_dimension))
+
+    return time_step, commanded_lon_vel, horizon, sample_length, number_samples, input_dimension, init_state, cart_mass, pendulum_mass, sample_input
+
+def test_mpc_inv_pend_model_init(model_init_params):
+    time_step, commanded_lon_vel, horizon, sample_length, number_samples, input_dimension, init_state, cart_mass, pendulum_mass, sample_input = model_init_params
+
+    inv_pendulum_model = InvPendulumModel(time_step, number_samples, sample_length, cart_mass, pendulum_mass)
+    return None
+
+def test_mpc_inv_pend_model_predict(model_init_params):
+    time_step, commanded_lon_vel, horizon, sample_length, number_samples, input_dimension, init_state, cart_mass, pendulum_mass, sample_input = model_init_params
+
+    inv_pendulum_model = InvPendulumModel(time_step, number_samples, sample_length, cart_mass, pendulum_mass)
+    sample_states = inv_pendulum_model.predict_states(init_state, sample_input)
+
+    assert sample_states.shape == (sample_length, number_samples, init_state.shape[0])
+    return None
+
