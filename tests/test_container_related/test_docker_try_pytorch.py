f4fa0a1368e639fb414476b194b520827240e087
RedLeaderOne <luc.coupal.1@ulaval.ca>
2022-03-17 13:40:07 -0400

    NLMPPI-37 | Implement is_run_in_DS_arm64_darwin_architecture() and pytest skip cuda pytorch test if run on Dockerized-SNOW arm64-Darwin architecture

--- tests/test_container_related/test_docker_try_pytorch.py
+++ tests/test_container_related/test_docker_try_pytorch.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import pytest
+
+from src.container_related.ds_python_utility import is_run_in_DS_arm64_darwin_architecture
 from src.container_related.try_pytorch import verify_pytorch_install, verify_pytorch_cuda_install
 
 
@@ -9,6 +11,8 @@ def test_verify_pytorch_install_PASS():
     return None
 
 
+@pytest.mark.skipif(is_run_in_DS_arm64_darwin_architecture(),
+                    reason="Cuda is not suported on Dockerized-SNOW container for arm64-Darwin architecture")
 def test_verify_pytorch_cuda_install():
     verify_pytorch_cuda_install()
     return None
